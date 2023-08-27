a99c0eb95370965474fdde7d0594653813adfd25
Martin Miglio <marmig0404@gmail.com>
2023-08-27 00:03:29 -0400

    make title bigger

--- src/components/page/NavBar.tsx
+++ src/components/page/NavBar.tsx
@@ -4,7 +4,7 @@ import Link from "next/link";
 export default function NavBar({ title }: { title: string }) {
   return (
     <div className="relative flex w-full justify-between py-4 items-center">
-      <h1 className="mx-auto text-3xl font-bold text-theme-600">
+      <h1 className="mx-auto text-4xl font-bold text-theme-600">
         <Link href="/">{title}</Link>
       </h1>
       <div className="absolute right-0">
