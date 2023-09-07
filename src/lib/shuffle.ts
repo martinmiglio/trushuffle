f209eecbef71f298a07bbf22b67b8c66651643d0
Martin Miglio <marmig0404@gmail.com>
2023-09-07 15:35:48 -0400

    add shuffle hook skelly

---
+++ src/lib/shuffle.ts
@@ -0,0 +1,15 @@
+export default function shuffle<T>(array: T[]) {
+  // Fisher–Yates shuffle
+  // https://stackoverflow.com/a/2450976
+  let currentIndex = array.length,
+    randomIndex;
+  while (currentIndex > 0) {
+    randomIndex = Math.floor(Math.random() * currentIndex);
+    currentIndex--;
+    [array[currentIndex], array[randomIndex]] = [
+      array[randomIndex],
+      array[currentIndex],
+    ];
+  }
+  return array;
+}
