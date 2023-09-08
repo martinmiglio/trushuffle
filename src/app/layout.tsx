8df51fa3f98dcb1a47a983e6d7b28dd88dd8e279
Martin Miglio <marmig0404@gmail.com>
2023-09-08 03:47:43 -0400

    add analytics

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,9 +3,11 @@ import authOptions from "@/app/api/auth/[...nextauth]/authOptions";
 import AuthSessionProvider from "@/components/auth/AuthSessionProvider";
 import FooterBar from "@/components/page/FooterBar";
 import NavBar from "@/components/page/NavBar";
+import { env } from "@/env/server";
 import type { Metadata } from "next";
 import { getServerSession } from "next-auth";
 import { Montserrat as Font } from "next/font/google";
+import Script from "next/script";
 
 const inter = Font({ subsets: ["latin"] });
 
@@ -13,6 +15,11 @@ export const metadata: Metadata = {
   title: "TruShuffle",
   description: "Truly random music shuffling for Spotify",
   icons: `/icon?v1`,
+  viewport: {
+    width: "device-width",
+    initialScale: 1,
+    maximumScale: 1,
+  },
 };
 
 export default async function RootLayout({
@@ -26,6 +33,13 @@ export default async function RootLayout({
       lang="en"
       className="bg-theme-200 dark:bg-theme-800 text-theme-800 dark:text-theme-200"
     >
+      <head>
+        <Script
+          async
+          src={env.ANALYTICS_URL}
+          data-website-id={env.ANALYTICS_ID}
+        />
+      </head>
       <AuthSessionProvider session={session}>
         <body className={inter.className}>
           <div className="mx-auto flex h-screen w-full max-w-screen-md flex-col justify-between px-4">
--- src/env/server.ts
+++ src/env/server.ts
@@ -5,9 +5,13 @@ export const env = createEnv({
   server: {
     SPOTIFY_CLIENT_ID: z.string(),
     SPOTIFY_CLIENT_SECRET: z.string(),
+    ANALYTICS_ID: z.string(),
+    ANALYTICS_URL: z.string(),
   },
   runtimeEnv: {
     SPOTIFY_CLIENT_ID: process.env.SPOTIFY_CLIENT_ID,
     SPOTIFY_CLIENT_SECRET: process.env.SPOTIFY_CLIENT_SECRET,
+    ANALYTICS_ID: process.env.ANALYTICS_ID,
+    ANALYTICS_URL: process.env.ANALYTICS_URL,
   },
 });
